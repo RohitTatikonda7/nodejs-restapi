86ac971b7b38d9edb444731a360d8993a87d8392
Rohit4072 <tatikonda.rohit@lumen.com>
2023-11-21 20:37:20 +0530

    user module
    user module

--- models/User.js
+++ models/User.js
@@ -2,16 +2,24 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
+    address: {
       type: String,
-      required: [true, "User name is required"],
-      unique: true,
+      required: [false, "Address is not required"],
+      unique: false,
     },
+    id: Number,
     email: {
       type: String,
       required: [true, "Email is required"],
       unique: true,
     },
+    username: { type: String, unique: true },
+    password: String,
+    name: {
+      firstname: String,
+      lastname: String
+    },
+    phone: { type: String, unique: true },
   },
   {
     timestamps: true,
